6dad68701a00d359bf5cc97156e39f341ce958ac
mosheurpau <mosheur.pau@gamil.com>
2022-03-30 15:51:02 +0600

    Create Simple Route and simple router setup

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Friends = () => {
+  return (
+    <div>
+      <h2>Hello Friends. How are you</h2>
+      <p>
+        <small>Parle kichu taka dhar dis..</small>
+      </p>
+    </div>
+  );
+};
+
+export default Friends;
