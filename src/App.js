340bda316ef6734eb179fb7d33f8e32dc801dc70
mosheurpau <mosheur.pau@gamil.com>
2022-03-30 16:15:03 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Friends from "./components/Friends/Friends";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my fancy Routing website!!!</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/friends" element={<Friends></Friends>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
