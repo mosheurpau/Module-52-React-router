6eb54ef39d7bf461bf96dc89119f763d099ae00e
mosheurpau <mosheur.pau@gamil.com>
2022-03-30 16:53:20 +0600

    Load Users, display users, dynamic link, use Navigate

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Friend from "../Friend/Friend";
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setFriends(data));
+  }, []);
   return (
     <div>
       <h2>Hello Friends. How are you</h2>
       <p>
-        <small>Parle kichu taka dhar dis..</small>
+        <small>Parle kichu taka dhar dis..: {friends.length}</small>
       </p>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend}></Friend>
+      ))}
     </div>
   );
 };
